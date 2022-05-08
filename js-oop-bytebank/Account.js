727293ebfb38ef0e30bb62bd2b96b09631c9c80e
Angela Caldas <aes.caldas@gmail.com>
2022-05-08 13:26:53 -0300

    Abstract class

--- js-oop-bytebank/Account.js
+++ js-oop-bytebank/Account.js
@@ -1,46 +1,54 @@
+// Abstract class: it works only for inheritance and never to be instanced
 export class Account {
   constructor(openingBalance, client, agency) {
-    this._balance = openingBalance
-    this._client = client
-    this._agency = agency
+    // avoiding instances of Account
+    if (this.constructor == Account) {
+      throw new Error(
+        'Você não deveria instanciar diretamente um objeto do tipo Conta.'
+      );
+    }
+
+    this._balance = openingBalance;
+    this._client = client;
+    this._agency = agency;
   }
 
   set client(newValue) {
     if (newValue instanceof Client) {
-      this._client = newValue
+      this._client = newValue;
     }
   }
 
   get client() {
-    return this._client
+    return this._client;
   }
 
   get balance() {
-    return this._balance
+    return this._balance;
   }
 
   withdraw(value) {
-    let tax = 1
-    return this._withdraw(value, tax)
+    let tax = 1;
+    return this._withdraw(value, tax);
   }
 
   _withdraw(value, tax) {
-    const withdrawal = tax * value
+    const withdrawal = tax * value;
     if (this._balance >= withdrawal) {
-      this._balance -= withdrawal
-      return withdrawal
+      this._balance -= withdrawal;
+      return withdrawal;
     }
 
-    return 0 // if the balance is under the withdrawal value, returns 0
+    return 0; // if the balance is under the withdrawal value, returns 0
   }
 
   deposit(value) {
-    if (value < 0) return
-    this._balance += value
+    if (value < 0) return;
+    this._balance += value;
   }
 
   transfer(value, account) {
-    const withdrawal = this.withdraw(value)
-    account.deposit(withdrawal)
+    const withdrawal = this.withdraw(value);
+    account.deposit(withdrawal);
   }
 }
--- js-oop-bytebank/index.js
+++ js-oop-bytebank/index.js
@@ -1,20 +1,17 @@
 // After saving each class in an unique file, we'll have inside index.js only the executions. And to make it work, we must create modules and import them inside this file
-import {Client} from './Client.js'
-import {CheckingAccount} from './CheckingAccount.js'
-import {SavingsAccount} from './SavingsAccount.js'
+import {Client} from './Client.js';
+import {CheckingAccount} from './CheckingAccount.js';
+import {SavingsAccount} from './SavingsAccount.js';
+import {Account} from './Account.js';
 
-const client1 = new Client('Angela', 11122233399)
+const client1 = new Client('Angela', 11122233399);
 // client1.name = 'Angela'
 // client1.cpf = 11122233399
 
-const account1 = new CheckingAccount(client1, 1001)
-account1.deposit(500)
-account1.withdraw(100)
+const account1 = new CheckingAccount(client1, 1001);
+const savingsAccount1 = new SavingsAccount(50, client1, 1001);
+const account = new Account(0, client1, 1001);
 
-const savingsAccount1 = new SavingsAccount(50, client1, 1001)
-savingsAccount1.withdraw(10)
-
-console.log(account1)
-console.log(savingsAccount1)
+console.log(account);
 
 // Since we're not linking our script to any HTML files, we must config our Node.js to understand index.js is a module. To run modules using NodeJs, we must initialize it with the command 'npm init' and then add the setup inside the 'package.json' file
