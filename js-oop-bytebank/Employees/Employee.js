58cbe5a7d29de3b8b346ec0c7b8976c4fe76c0c4
Angela Caldas <aes.caldas@gmail.com>
2022-05-09 14:57:32 -0300

    Understanding polymorphism

--- js-oop-bytebank/Employees/Employee.js
+++ js-oop-bytebank/Employees/Employee.js
@@ -5,5 +5,14 @@ export class Employee {
     this._cpf = cpf;
 
     this._bonus = 1;
+    this._password;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  createPassword(password) {
+    this._password = password;
   }
 }
--- js-oop-bytebank/index.js
+++ js-oop-bytebank/index.js
@@ -1,19 +1,13 @@
-// After saving each class in an unique file, we'll have inside index.js only the executions. And to make it work, we must create modules and import them inside this file
 import {Client} from './Client.js';
-import {CheckingAccount} from './Accounts/CheckingAccount.js';
-import {SavingsAccount} from './Accounts/SavingsAccount.js';
-import {SalaryAccount} from './Accounts/SalaryAccount.js';
+import {Manager} from './Employees/Manager.js';
+import {Director} from './Employees/Director.js';
+import {Authentication} from './Authentication.js';
 
-const client1 = new Client('Angela', 11122233399);
-// client1.name = 'Angela'
-// client1.cpf = 11122233399
+const director = new Director('Rodrigo', 10000, 12345678900);
+director.createPassword('123456789');
+const manager = new Manager('Ricardo', 5000, 78945612301);
+manager.createPassword('123');
 
-const account1 = new CheckingAccount(client1, 1001);
-const savingsAccount1 = new SavingsAccount(50, client1, 1001);
-const salaryAccount = new SalaryAccount(client1);
-salaryAccount.deposit(100);
-salaryAccount.withdraw(10);
+const isLogged = Authentication.login(manager, '123');
 
-console.log(salaryAccount);
-
-// Since we're not linking our script to any HTML files, we must config our Node.js to understand index.js is a module. To run modules using NodeJs, we must initialize it with the command 'npm init' and then add the setup inside the 'package.json' file
+console.log(isLogged);
