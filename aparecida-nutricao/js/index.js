7f64b9db800eca5596b829bc75ebde1c074917a1
Angela Caldas <aes.caldas@gmail.com>
2022-06-14 12:55:56 -0300

    Add form to add new patient

--- aparecida-nutricao/js/index.js
+++ aparecida-nutricao/js/index.js
@@ -33,3 +33,37 @@ for (let i = 0; i < pacientes.length; i++) {
     tdImc.textContent = imc.toFixed(2);
   }
 }
+
+const botaoAdicionar = document.querySelector('#adicionar-paciente');
+
+botaoAdicionar.addEventListener('click', e => {
+  e.preventDefault();
+
+  const form = document.querySelector('#form-adiciona');
+  const tabela = document.querySelector('#tabela-pacientes');
+
+  let nome = form.nome.value;
+  let peso = form.peso.value;
+  let altura = form.altura.value;
+  let gordura = form.gordura.value;
+
+  let pacienteTr = document.createElement('tr');
+  let nomeTd = document.createElement('td');
+  let pesoTd = document.createElement('td');
+  let alturaTd = document.createElement('td');
+  let gorduraTd = document.createElement('td');
+  let imcTd = document.createElement('td');
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  tabela.appendChild(pacienteTr);
+});
